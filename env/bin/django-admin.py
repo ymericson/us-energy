916a0a5f98c3938ff20910071126df21ed1e3a3f
Eric Son <yson@cs.uchicago.edu>
2020-07-16 00:04:48 -0500

    update env

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/student/capp30122-win-20-ldinh-yson-zhange/project/ui/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
